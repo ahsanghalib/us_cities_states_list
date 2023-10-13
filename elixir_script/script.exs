44fe6141f3b9b3ec01fa0d46b8b289030afdd4e2
ag <me@ag.com>
2023-10-13 20:51:35 +0500

    elixir script

---
+++ elixir_script/script.exs
@@ -0,0 +1,55 @@
+#!/usr/bin/env elixir
+
+defmodule Benchmark do
+  def measure(function) do
+    time =
+      function
+      |> :timer.tc()
+      |> elem(0)
+      |> Kernel./(1_000_000)
+      |> to_string()
+
+    IO.puts(time <> " sec")
+  end
+end
+
+defmodule Solution do
+  def process() do
+    list =
+      File.stream!("./us_cities_states_counties.csv")
+      |> Stream.map(&String.trim(&1))
+      |> Stream.map(&String.split(&1, "|"))
+      |> Stream.filter(fn
+        ["city" | _] -> false
+        _ -> true
+      end)
+
+    cities =
+      list
+      |> Stream.map(&(Stream.drop(&1, -2) |> Enum.to_list()))
+      |> Enum.uniq()
+      |> Enum.map(fn e ->
+        %{
+          city: Enum.at(e, 0),
+          state_short: Enum.at(e, 1),
+          state_full: Enum.at(e, 2)
+        }
+      end)
+
+    states =
+      list
+      |> Stream.map(fn l -> l |> Enum.to_list() |> Enum.slice(1, 2) end)
+      |> Enum.uniq()
+      |> Enum.map(fn e ->
+        %{
+          state_short: Enum.at(e, 0),
+          state_full: Enum.at(e, 1)
+        }
+      end)
+
+    IO.inspect(length(cities))
+    IO.inspect(length(states))
+  end
+end
+
+Benchmark.measure(&Solution.process/0)
